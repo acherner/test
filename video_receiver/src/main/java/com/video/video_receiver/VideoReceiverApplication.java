38c92f55e1bba84a0750adc311f251e602e30b7f
anatolyc <anatolyc@mprest.com>
2018-12-17 11:32:15 +0200

    test commit

---
+++ video_receiver/src/main/java/com/video/video_receiver/VideoReceiverApplication.java
@@ -0,0 +1,14 @@
+package com.video.video_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoReceiverApplication.class, args);
+    }
+
+}
+
